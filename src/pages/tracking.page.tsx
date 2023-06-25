2ef0e6b5d419dd12c8a239cfa4898a21ebe3c4f7
Eric Le Donge <eric.ledonge@gmail.com>
2023-06-25 15:26:15 -0400

    :sparkles: Tracking

--- src/pages/tracking.page.tsx
+++ src/pages/tracking.page.tsx
@@ -1,3 +1,36 @@
+import { Button, createStyles, Select } from "@mantine/core";
+
+const dataCompany = ["Béton Provincial", "HRCOM", "Partage Club"];
+
+const dataActivity = ["Activity 1", "Activity 2", "Activity 3"];
+
 export const TrackingPage = () => {
-  return <div>Tracking</div>;
+  const { classes } = useStyles();
+
+  return (
+    <>
+      <h1>Tracking</h1>
+      <section className={classes.formContainer}>
+        <Select label="Company" placeholder="Pick one" data={dataCompany} />
+
+        <Select label="Activity" placeholder="Pick one" data={dataActivity} />
+
+        <Button>Start</Button>
+      </section>
+    </>
+  );
 };
+
+const useStyles = createStyles((theme) => ({
+  formContainer: {
+    display: "flex",
+    flexDirection: "column",
+    gap: 20,
+
+    [theme.fn.largerThan("sm")]: {
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "flex-end",
+    },
+  },
+}));
