016a83e60ede7275b913da684172a5b0c42b55fd
Eric Le Donge <eric.ledonge@gmail.com>
2023-07-01 15:20:06 +0200

    :construction:

--- src/pages/tracking.page.tsx
+++ src/pages/tracking.page.tsx
@@ -1,4 +1,6 @@
-import { Button, createStyles, Select } from "@mantine/core";
+import { useState } from "react";
+import { Button, createStyles, Select, Text } from "@mantine/core";
+import { useStopwatch } from "react-timer-hook";
 
 const dataCompany = ["Béton Provincial", "HRCOM", "Partage Club"];
 
@@ -7,21 +9,58 @@ const dataActivity = ["Activity 1", "Activity 2", "Activity 3"];
 export const TrackingPage = () => {
   const { classes } = useStyles();
 
+  const [selectedCompany, setSelectedCompany] = useState();
+  const [selectedActivity, setSelectedActivity] = useState();
+  const [startingDate, setStartingDate] = useState<Date>();
+
+  const { minutes, hours, isRunning, start, pause, reset } = useStopwatch();
+
+  const handleClickStart = () => {
+    setStartingDate(new Date());
+    start();
+  };
+
+  const handleClickPause = () => {
+    pause();
+  };
+
+  const handleStop = () => {
+    pause();
+  };
+
   return (
-    <>
+    <div className={classes.mainContainer}>
       <h1 style={{ textAlign: "center" }}>Tracking</h1>
-      <section className={classes.formContainer}>
+
+      <div className={classes.formContainer}>
         <Select label="Company" placeholder="Pick one" data={dataCompany} />
-
         <Select label="Activity" placeholder="Pick one" data={dataActivity} />
+      </div>
 
-        <Button>Start</Button>
-      </section>
-    </>
+      <div className={classes.buttonContainer}>
+        <Button onClick={handleClickStart}>Start</Button>
+        <Button onClick={handleClickPause} variant="default">
+          Pause
+        </Button>
+        <Button onClick={handleClickStart} color="red">
+          Stop
+        </Button>
+      </div>
+
+      <div className={classes.formContainer}>
+        <Text>Started at: {startingDate?.toDateString()}</Text>
+        <Text>{`Running since: ${hours} hours : ${minutes} minutes`}</Text>
+      </div>
+    </div>
   );
 };
 
 const useStyles = createStyles((theme) => ({
+  mainContainer: {
+    display: "flex",
+    flexDirection: "column",
+    gap: 20,
+  },
   formContainer: {
     display: "flex",
     flexDirection: "column",
@@ -32,6 +71,16 @@ const useStyles = createStyles((theme) => ({
       flexDirection: "row",
       justifyContent: "center",
       alignItems: "flex-end",
+    },
+  },
+  buttonContainer: {
+    display: "flex",
+    justifyContent: "center",
+    gap: 20,
+  },
+  button: {
+    [theme.fn.smallerThan("sm")]: {
+      minWidth: 150,
     },
   },
 }));
